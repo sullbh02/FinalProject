5a166531b1f5e67b8f53ec294e6044cfd3f2fc45
WSBSULLIVAN-LHS\bsullivan <sullbh02@students.ipfw.edu>
2017-11-22 15:23:13 -0500

    Add Login Panel
    Adding a login screen to the main form as a requirment for this project.  The username and password will be stored in the database and validated.

---
+++ Hoard/Hoard/MainForm.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Hoard
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewTicket = new System.Windows.Forms.Button();
+            this.btnNewAsset = new System.Windows.Forms.Button();
+            this.dgvRecentTickets = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsTicketsForm = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsAssetsForm = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecentTickets)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNewTicket
+            // 
+            this.btnNewTicket.Location = new System.Drawing.Point(120, 164);
+            this.btnNewTicket.Name = "btnNewTicket";
+            this.btnNewTicket.Size = new System.Drawing.Size(110, 23);
+            this.btnNewTicket.TabIndex = 0;
+            this.btnNewTicket.Text = "Create Ticket";
+            this.btnNewTicket.UseVisualStyleBackColor = true;
+            this.btnNewTicket.Click += new System.EventHandler(this.btnNewTicket_Click);
+            // 
+            // btnNewAsset
+            // 
+            this.btnNewAsset.Location = new System.Drawing.Point(236, 164);
+            this.btnNewAsset.Name = "btnNewAsset";
+            this.btnNewAsset.Size = new System.Drawing.Size(110, 23);
+            this.btnNewAsset.TabIndex = 1;
+            this.btnNewAsset.Text = "Add Asset ";
+            this.btnNewAsset.UseVisualStyleBackColor = true;
+            this.btnNewAsset.Click += new System.EventHandler(this.btnNewAsset_Click);
+            // 
+            // dgvRecentTickets
+            // 
+            this.dgvRecentTickets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecentTickets.Location = new System.Drawing.Point(23, 33);
+            this.dgvRecentTickets.Name = "dgvRecentTickets";
+            this.dgvRecentTickets.Size = new System.Drawing.Size(418, 125);
+            this.dgvRecentTickets.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 402);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(560, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.menuStrip1);
+            this.panelMain.Controls.Add(this.dgvRecentTickets);
+            this.panelMain.Controls.Add(this.btnNewAsset);
+            this.panelMain.Controls.Add(this.btnNewTicket);
+            this.panelMain.Location = new System.Drawing.Point(44, 27);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(462, 197);
+            this.panelMain.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsQuit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // tsQuit
+            // 
+            this.tsQuit.Name = "tsQuit";
+            this.tsQuit.Size = new System.Drawing.Size(97, 22);
+            this.tsQuit.Text = "Quit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsTicketsForm,
+            this.tsAssetsForm});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // tsTicketsForm
+            // 
+            this.tsTicketsForm.Name = "tsTicketsForm";
+            this.tsTicketsForm.Size = new System.Drawing.Size(111, 22);
+            this.tsTicketsForm.Text = "Tickets";
+            // 
+            // tsAssetsForm
+            // 
+            this.tsAssetsForm.Name = "tsAssetsForm";
+            this.tsAssetsForm.Size = new System.Drawing.Size(111, 22);
+            this.tsAssetsForm.Text = "Assets";
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Controls.Add(this.label2);
+            this.panelLogin.Controls.Add(this.label1);
+            this.panelLogin.Controls.Add(this.txtPassword);
+            this.panelLogin.Controls.Add(this.txtUsername);
+            this.panelLogin.Controls.Add(this.btnLogin);
+            this.panelLogin.Location = new System.Drawing.Point(91, 230);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(360, 131);
+            this.panelLogin.TabIndex = 6;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(146, 78);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(136, 26);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(136, 52);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 424);
+            this.Controls.Add(this.panelLogin);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Hoard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecentTickets)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewTicket;
+        private System.Windows.Forms.Button btnNewAsset;
+        private System.Windows.Forms.DataGridView dgvRecentTickets;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsQuit;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsTicketsForm;
+        private System.Windows.Forms.ToolStripMenuItem tsAssetsForm;
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Hoard/Hoard/MainForm.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Ben Sullivan 
+ * Semester Project - Hoard
+ * An application that will create and store tickets in a database.  Also the application will allow for assest tracking by storing assests in a database.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hoard
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            Load += new EventHandler(Form1_Load);
+            InitializeComponent();
+        }
+
+        // add login table adapter
+        private LoginDataSetTableAdapters.UsersTableAdapter loginAdapter = new LoginDataSetTableAdapters.UsersTableAdapter();
+
+        private void tsTicketsForm_Click(object sender, EventArgs e)
+        {
+            // show tickets form
+            Tickets ticketsForm = new Tickets();
+            ticketsForm.Show();
+        }
+
+        private void tsAssetsForm_Click(object sender, EventArgs e)
+        {
+            // show assets form
+            Assets assestsForm = new Hoard.Assets();
+            assestsForm.Show();
+        }
+
+        private void btnNewAsset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNewTicket_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsQuit_Click(object sender, EventArgs e)
+        {
+            // close program
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //on form load fill the form with the login panel
+            panelLogin.Dock = DockStyle.Fill;
+
+            //Hide the main panel until the user has succesfully logged in
+            panelMain.Hide();
+            this.AcceptButton = btnLogin;
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //vars for the login panel
+            string username = "";
+            string password = "";
+            bool validUser = false;
+            bool validPass = false;
+           
+
+            // check if the user entered a valid username
+
+            if (txtUsername.Text.Length > 0)
+            {
+             
+                validUser = true;
+            }
+            else
+            {
+                lblStatus.Text = "Please enter a valid username";
+            }
+
+            // check for a valid password
+
+            if(txtPassword.Text.Length > 0)
+            {
+                         validPass = true;
+            }
+            else
+            {
+                lblStatus.Text = "Pleas enter a valid password";
+            }
+
+            //if both the username and password are valid continue
+            if(validUser == true && validPass == true)
+            {
+                txtPassword.Text = password;
+                txtUsername.Text = username;
+                try
+                {
+                    // check if the username and password combo is in our database
+                    if(loginAdapter.Search(loginAdapter.GetData(), username, password) > 0)
+                    {
+                        // hide the login panel and display the main panel
+                        panelLogin.Hide();
+                        panelMain.Show();
+                        panelMain.Dock = DockStyle.Fill;
+                        this.AcceptButton = null;
+                        txtPassword.Clear();
+                        txtUsername.Clear();
+                    }
+                    // if the combo is invalid search for username in database
+                    else if(loginAdapter.SearchUsername(loginAdapter.GetData(), username) > 0)
+                    {
+                        // if username is found inform user of an invalid password
+                        lblStatus.Text = "Invalid password";
+                    }
+                    // if username is not found inform user of invalid crendtials
+                    else
+                    {
+                        lblStatus.Text = "Invalid credentials";
+                    }
+                }
+                catch
+                {
+                    lblStatus.Text = "Error logging in";
+                }
+            }
+
+
+
+        }
+    }
+}
